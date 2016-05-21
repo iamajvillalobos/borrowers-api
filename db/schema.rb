cda813771f569a059d1c91452684d0fe6f0a3ad2
Arman Jon Villalobos <me@armanjon.com>
2016-05-21 20:39:32 +0800

    created an article model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160520130131) do
+ActiveRecord::Schema.define(version: 20160521123905) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "available"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "friends", force: :cascade do |t|
     t.string   "first_name"
