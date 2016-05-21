7f0af7aa564609b71fe11b8d9f896d84460572d9
Arman Jon Villalobos <me@armanjon.com>
2016-05-21 20:06:13 +0800

    add validation for creating a friend

--- test/models/friend_test.rb
+++ test/models/friend_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class FriendTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "requires first name" do
+    friend = Friend.new(last_name: "Alcano", email: "christ@alcano.com")
+    assert_not friend.save, "Saved friend without first_name"
+    assert_not_empty friend.errors[:first_name]
+  end
+
+  test "requires last name" do
+    friend = Friend.new(first_name: "Christa", email: "christ@alcano.com")
+    assert_not friend.save, "Saved friend without last_name"
+    assert_not_empty friend.errors[:last_name]
+  end
+
+  test "requires email" do
+    friend = Friend.new(first_name: "Christa", last_name: "Alcano")
+    assert_not friend.save, "Saved friend without email"
+    assert_not_empty friend.errors[:email]
+  end
+
 end
